c58b2aaac49b5ce21247d42d85ccd3c78c2de8b9
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2023-05-10 21:48:02 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    function car_motor(ul: number = 0, ur: number = 0, ll: number = 0, lr: number = 0) {
+        ul = Math.map(ll, -100, -100, -255, 255)
+        ur = Math.map(lr, -100, -100, -215, 215)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, ul)
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, -ur)
+    }
+})
