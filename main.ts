47d7fd8d879869b996af9e4dc5724389a64e4586
Jakubpesser <115145147+Jakubpesser@users.noreply.github.com>
2023-06-20 07:35:33 +0200

    Aktualizovat pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,12 @@ radio.setFrequencyBand(7)
 
 let lastBtnA = false
 let lastBtnB = false
+let lastLOGO = false
 
 basic.forever(function() {
     let btnA = input.buttonIsPressed(Button.A)
     let btnB = input.buttonIsPressed(Button.B)
+    let btnLOGO = input.logoIsPressed()
 
     if (btnA != lastBtnA) {
         radio.sendValue("btnA", +btnA)
@@ -16,6 +18,11 @@ basic.forever(function() {
         radio.sendValue("btnB", +btnB)
     }
 
+    if (btnLOGO != lastLOGO) {
+        radio.sendValue("btnLOGO", +btnLOGO)
+    }
+
     lastBtnA = btnA
     lastBtnB = btnB
+    lastLOGO = btnLOGO
 })
